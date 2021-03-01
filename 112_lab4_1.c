7cfd7cbc86aa8fac4ae09ddf73d6b0dbcfd96ffc
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-01 19:31:27 +0700

    Medal

--- 112_lab4_1.c
+++ 112_lab4_1.c
@@ -3,9 +3,22 @@ int main()
 {
     int n;
     scanf("%d" ,&n);
-    int a1[n][3],a2[n],b,c,i,t,max=0,min=2000;
+    int a1[n][3],a2[n],b,c,i,count,max=0,min=2000;
     for(i=0;i<n;i++)
     {
-
+        scanf("%d %d %d" ,&a1[i][0],&a1[i][1],&a1[i][2]);
+    }
+    for(i=0;i<n;i++)
+    {
+        a2[i]=(a1[i][0]*4)+(a1[i][1]*2)+a1[n][2];
+    }
+    for(i=0;i<n-2;i++)
+    {
+        count=a2[i]+a2[i+1]+a2[i+2];
+        if(max<=count)
+        {
+            b=i+1;
+            max=count;
+        }
     }
 }
