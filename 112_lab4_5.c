2c9d79b85ff03f3fd4355c356fab98b87689ff61
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-01 17:56:45 +0700

    Acronym

--- 112_lab4_5.c
+++ 112_lab4_5.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<string.h>
+void Acronym(char*s);
 int main()
 {
-
+    char sentence[200];
+    printf("Enter sentence: ");
+    gets(sentence);
+    Acronym(sentence);
 }
