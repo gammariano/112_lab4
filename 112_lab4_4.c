0d78cf66b9bc6b2d859544f8cdbdf0629b09017b
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-01 18:58:30 +0700

    Acronym

--- 112_lab4_4.c
+++ 112_lab4_4.c
@@ -5,5 +5,16 @@ int main()
     char s[1000],t[15][5]={"i","of","the","on","at","for","with","a","an","in","and"};
     char *p;
     int i=1,j,k=0;
-
+    scanf("%[^\n]s" ,s);
+    p=strtok(s," ");
+    while(p!=NULL)
+    {
+        for(j=0;j<15;j++)
+        {
+            if(strcmp(p,t[j]))
+            {
+                k++;
+            }
+        }
+    }
 }
