bdcb007666b64c3a4326e2f936ad41c543411466
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-01 19:12:06 +0700

    Acronym

--- 112_lab4_4.c
+++ 112_lab4_4.c
@@ -1,28 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<string.h>
 #include<ctype.h>
 int main()
 {
-    char s[1000],t[11][5]={"i","of","the","on","at","for","with","a","an","in","and"};
+    char s[1000] , t[11][5]={"i","of","the","on","at","for","with","a","an","in","and"};
     char *p;
     int i=0,j,k=0;
-    scanf("%[^\n]s" ,s);
+    scanf("%[^\n]s",s);
     p=strtok(s," ");
     while(p!=NULL)
     {
         for(j=0;j<11;j++)
-        {
             if(strcmp(p,t[j]))
-            {
                 k++;
-            }
-        }
         if(k==11||i==0)
-        {
-            printf("%c" ,toupper(p[0]));
+            printf("%c",toupper(p[0]));
             p=strtok(NULL," ");
             i++, k=0;
-        }
     }
     return 0;
 }
