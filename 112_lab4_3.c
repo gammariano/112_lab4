2d5574fb360050d86ababaa2641f828fd26d6b8b
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-01 17:20:03 +0700

    แป้นพิมพ์สกปรก

--- 112_lab4_3.c
+++ 112_lab4_3.c
@@ -5,5 +5,13 @@ int main()
     char a[200];
     scanf("%s" ,a);
     i=strlen(a);
+    while(key<i)
+    {
+        if(a[key-1]!=a[key])
+        {
+            printf("%c" ,a[key]);
+        }
+        key++;
+    }
     return 0;
 }
