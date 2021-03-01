78fa3107151743f52734972d9671b864d91862cc
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-01 17:57:42 +0700

    Acronym

--- 112_lab4_5.c
+++ 112_lab4_5.c
@@ -8,3 +8,11 @@ int main()
     gets(sentence);
     Acronym(sentence);
 }
+void Acronym (char *s)
+{
+    int i=1;
+    char delim[]=" ";
+    char *j;
+    j=strtok(s,delim);
+    printf("%c",toupper(s[0]));
+}
