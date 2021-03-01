4df2a1bfa95e7125eb7367d4afdf142504ed5a5e
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-01 17:18:52 +0700

    แป้นพิมพ์สกปรก

--- 112_lab4_3.c
+++ 112_lab4_3.c
@@ -1,17 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int i;
-    char a[500],key;
+    int i,key=0;
+    char a[200];
     scanf("%s" ,a);
-    while(a[i]!='\0')
-    {
-        key=a[i];
-        printf("%c" ,a[i]);
-        while(a[i]==key)
-        {
-            i++;
-        }
-    }
+    i=strlen(a);
     return 0;
 }
