138827eb123328d2c615be4f690e5972ebdf10b2
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-01 18:36:09 +0700

    Four glass

--- 112_lab4_5.c
+++ 112_lab4_5.c
@@ -8,4 +8,14 @@ int main()
     {
         scanf("%[^\n]s" ,glass[i]);
     }
+    for(j=0;j<n;j++)
+    {
+        for(i=0;glass[j][i]!='\0';i++)
+        {
+            if(glass[j][i]>='A'&&glass[j][i]<='E')
+            {
+
+            }
+        }
+    }
 }
